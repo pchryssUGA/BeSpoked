6e6caa96fd52262a618009a98349f05e66c5012a
Philip <philipchryssochoos@gmail.com>
2024-07-27 13:03:21 -0400

    Register DB Models

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,27 @@
 from django.contrib import admin
-from .models import TestTable
+from .models import Product, Salesperson, Customer, Sale, Discount
 
 # Register your models here.
-
-@admin.register(TestTable)
-class TestTableAdmin(admin.ModelAdmin):
-    list_display = ('ID', "Name")
+    
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'manufacturer', 'style', 'purchase_price', 
+                    'sale_price', 'quantity_on_hand', 'commision_percentage')
+    
+@admin.register(Salesperson)
+class SalespersonAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'address', 'phone',
+                    'start_date', 'termination_date', 'manager')
+    
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'address', 'phone', 'start_date')
+    
+@admin.register(Sale)
+class SaleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product', 'salesperson', 'customer', 'sale_date')
+    
+@admin.register(Discount)
+class DiscountAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product', 'start_date', 'end_date', 'discount_percentage')
+    
--- app/models.py
+++ app/models.py
@@ -1,11 +1,70 @@
 from django.db import models
 
-# Create your models here.
-class TestTable(models.Model):
-    ID = models.IntegerField(primary_key=True)
-    Name = models.CharField(max_length=10)
+# Product Model
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    manufacturer = models.CharField(max_length=50)
+    style = models.CharField(max_length=50)
+    purchase_price = models.DecimalField(max_digits=8, decimal_places=2)
+    sale_price = models.DecimalField(max_digits=8, decimal_places=2)
+    quantity_on_hand = models.IntegerField()
+    commision_percentage = models.DecimalField(max_digits=5, decimal_places=2)
+
+    class Meta:
+        db_table = 'Product'
+        managed = False
+        
+    def __str__(self):
+        return str(self.id)+": "+self.name
+        
+# Salesperson Model
+class Salesperson(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    phone = models.CharField(max_length=15)
+    start_date = models.DateField()
+    termination_date = models.DateField(blank=True, null=True)
+    manager = models.CharField(max_length=50)
     
     class Meta:
-        db_table = 'test_table'
+        db_table = 'Salesperson'
         managed = False
+        
+    def __str__(self):
+        return str(self.id)+": "+self.first_name+" "+self.last_name
+
+class Customer(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    phone = models.CharField(max_length=15)
+    start_date = models.DateField()
     
+    class Meta:
+        db_table = 'Customer'
+        managed = False
+        
+    def __str__(self):
+        return str(self.id)+": "+self.first_name+" "+self.last_name
+
+class Sale(models.Model):
+    id = models.AutoField(primary_key=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    salesperson = models.ForeignKey(Salesperson, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    sale_date = models.DateField()
+    
+class Discount(models.Model):
+    id = models.AutoField(primary_key=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    discount_percentage = models.DecimalField(max_digits=5, decimal_places=2)
+   
+    
+    
+
